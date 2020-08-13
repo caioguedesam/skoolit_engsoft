c3eb6e572e3c5df715719b7dcf06bc67baf2ff1d
Guilherme Bezerra <40393853+guilhermebezerrads@users.noreply.github.com>
2020-08-13 08:18:37 -0300

    Criação do banco de dados
    Agora é possível adicionar um novo usuário no banco de dados.

--- skoolit/views.py
+++ skoolit/views.py
@@ -1,6 +1,27 @@
-from flask import render_template
-from skoolit import app
+from flask import render_template, redirect, url_for
+from skoolit import app, models, forms, db
 
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+@app.route('/criar', methods=['POST', 'GET'])
+def criar():
+	form = forms.CriarUsuarioForm()
+
+	if form.validate_on_submit():
+		novo_usuario = models.Usuario(form.email.data)
+		db.session.add(novo_usuario)
+		db.session.commit()
+
+		return redirect(url_for('listar'))
+
+	return render_template('criar.html', form=form)
+
+
+@app.route('/listar', methods=['POST', 'GET'])
+def listar():
+
+	usuarios = models.Usuario.query.all()
+
+	return render_template('listar.html', usuarios=usuarios)
