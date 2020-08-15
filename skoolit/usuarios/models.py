005a5fb800b1bcd8c7f999ea3c697cf6b85660c3
arthursl12 <arthursoutolima@gmail.com>
2020-08-15 08:56:29 -0300

    Adaptação classe Usuario (WIP) - Adaptação da classe usuário com os novos atributos - Criação de um script para importar um banco de usuários aleatórios - Necessidade de correção: import utilizou os campos errados

--- skoolit/usuarios/models.py
+++ skoolit/usuarios/models.py
@@ -4,7 +4,6 @@ from skoolit import db
 class Usuario(db.Model):
 
 	__tablename__ = 'usuarios'
-
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String, nullable=False)
 	papel = db.Column(db.String)
