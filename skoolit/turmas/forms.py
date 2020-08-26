64b7e51d8c796b472626b12df41deba0d3b721ce
arthursl12 <arthursoutolima@gmail.com>
2020-08-26 15:31:43 -0300

    Organizando o merge

--- skoolit/turmas/forms.py
+++ skoolit/turmas/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, IntegerField
 from wtforms.validators import DataRequired
 
 class CriarMateriaForm(FlaskForm):
