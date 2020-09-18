5f208d5323f545bd957a921ba485bee2a39c162e
arthursl12 <arthursoutolima@gmail.com>
2020-09-18 19:12:04 -0300

    Home é dividida entre usuários diferentes

--- skoolit/views/home.py
+++ skoolit/views/home.py
@@ -1,12 +1,26 @@
 from flask import render_template, redirect, url_for, g
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 #local imports
 from skoolit import app
+from skoolit.models import Usuario
 
-@app.route('/')
-@app.route('/index')
+@app.route('/home')
 @login_required
 def home():
-	return render_template('home.html')
-	
+	userTipo = Usuario.dbGetUser(current_user.get_id()).papel
+	if userTipo == 'adm':
+		return render_template('home.html') 	#Home do admin
+	elif userTipo == 'prof':
+		return render_template('home.html')		#Home do prof
+	elif userTipo == 'al':
+		return render_template('home.html')		#Home do aluno
+	else:
+		print('Papel de usuário não encontrado!')
+		return 404
+
+
+# Página inicial bonita
+# @app.route('/')
+# def landing():
+# 	return render_template('home.html')
